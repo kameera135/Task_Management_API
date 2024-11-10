89dcbaaf4a078e1c1780d4525a4f714e60b4bb72
kameera135 <kameera135@users.noreply.github.com>
2024-11-10 14:17:58 +0530

    change the mongo connection request

--- src/models/index.js
+++ src/models/index.js
@@ -2,10 +2,20 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 
-try {
-    mongoose.connect(`${process.env.dbUrl}/${process.env.dbName}`)
-    console.log("mongoose connected ")
-} catch (error) {
-    console.log(error)
-}
+// try {
+//     mongoose.connect(`${process.env.dbUrl}/${process.env.dbName}`)
+//     console.log("mongoose connected ")
+// } catch (error) {
+//     console.log(error)
+// }
+
+const mongoUri = `${process.env.dbUrl}/${process.env.dbName}`;
+
+console.log("URL",mongoUri);
+
+mongoose.connect(mongoUri)
+.then(() => console.log('mongoos connected'))
+.catch((err) => console.log("mongoos connection error: ", err))
+
+
 export default mongoose
